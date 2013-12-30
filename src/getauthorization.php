fc828477a4bc543586d823808500376ca57a444d
Mr. Snowf1ake <mistahsnowflake@gmail.com>
2013-12-29 21:28:10 -0500

    Replace all tabs with spaces
    The code used \t instead of four spaces in some instances. This
    has been replaced to maintain code consistency.OA

--- src/getauthorization.php
+++ src/getauthorization.php
@@ -32,24 +32,24 @@ if (isset($_REQUEST['oauth_token'])) {
       <h1>Twitter Git Hook Experiment</h1>
       <h2>Thanks for being a part of this!</h2>
       <?php
-	 if ($redir) {
-	 echo '<p><b>Your generated credentials will download in 5 seconds</b></p>';
-	 }
+     if ($redir) {
+     echo '<p><b>Your generated credentials will download in 5 seconds</b></p>';
+     }
       ?>
       <p>
-	So, you've gone ahead and downloaded the zip file? Extract the contents
-	into your project's `.git/hooks` directory. Make sure that post-commit is
-	executable (and if it is not, run a <span style='color:blue'>chmod +x post-commit</span>).
-	<span style='color:red'> If you already have a post-commit hook, please
-	rename this and call it from your main post-commit.</span>
+    So, you've gone ahead and downloaded the zip file? Extract the contents
+    into your project's `.git/hooks` directory. Make sure that post-commit is
+    executable (and if it is not, run a <span style='color:blue'>chmod +x post-commit</span>).
+    <span style='color:red'> If you already have a post-commit hook, please
+    rename this and call it from your main post-commit.</span>
       </p>
       <p>
-	Let's reiterate how to use this script... In your git commit message,
-	surround text you want to send to Twitter with `_*` and `*_` like this:
-	<pre>Initialize project _*Working on an awesome new project!*_</pre>
-	If you have multiple blocks like that, it will send off multiple posts
-	to Twitter. If you make the line too long, the script will split it up
-	into multiple posts before sending it off to Twitter. Give it a shot!
+    Let's reiterate how to use this script... In your git commit message,
+    surround text you want to send to Twitter with `_*` and `*_` like this:
+    <pre>Initialize project _*Working on an awesome new project!*_</pre>
+    If you have multiple blocks like that, it will send off multiple posts
+    to Twitter. If you make the line too long, the script will split it up
+    into multiple posts before sending it off to Twitter. Give it a shot!
       </p>
   </body>
 </html>
--- src/index.php
+++ src/index.php
@@ -23,44 +23,44 @@ $_SESSION['token_secret'] = $temporary_credentials['oauth_token_secret'];
     <a href='https://github.com/mrsnowf1ake/twitter-git-hook'>https://github.com/mrsnowf1ake/twitter-git-hook</a>
     </p>
     <p>
-	We all know that writing documentation and updates sucks.
-	Like, how often did you begin working on a new project being like
-	"hey, imma gonna write updates and docs on this" and then a week
-	later be like "... this is too hard, imma give up"? Anyway, nothing
-	I can do about your laziness with docs... But updates? How about
-	embed them into your commit messages? (You better write real commit
-	messages... If you don't, you and I will never get along. I promise :D)
-	What if you could just write <pre>me@swag$ git commit -m "Fix suckish bug _*Last bug squashed, deep fried, then digested. Release on Saturday*_"</pre> and
-	everyone on Twitter saw your nasty comment about eating bugs? I would be
-	totally down for that.
+    We all know that writing documentation and updates sucks.
+    Like, how often did you begin working on a new project being like
+    "hey, imma gonna write updates and docs on this" and then a week
+    later be like "... this is too hard, imma give up"? Anyway, nothing
+    I can do about your laziness with docs... But updates? How about
+    embed them into your commit messages? (You better write real commit
+    messages... If you don't, you and I will never get along. I promise :D)
+    What if you could just write <pre>me@swag$ git commit -m "Fix suckish bug _*Last bug squashed, deep fried, then digested. Release on Saturday*_"</pre> and
+    everyone on Twitter saw your nasty comment about eating bugs? I would be
+    totally down for that.
       </p>
       <p>
-	So, help me with this little experiment. Authorize this program with
-	Twitter (but make another account... I can't afford SSL, and besides,
-	you already know you can't trust me since I suck at documentation)
-	below, put the automatically downloaded file and git hook into the
-	git hook directory in your project, and then give it a shot.
+    So, help me with this little experiment. Authorize this program with
+    Twitter (but make another account... I can't afford SSL, and besides,
+    you already know you can't trust me since I suck at documentation)
+    below, put the automatically downloaded file and git hook into the
+    git hook directory in your project, and then give it a shot.
       </p>
       <p>
-	Also, just a warning, I'm adding a tag #githook so I can track your
-	usage of it. I don't mean to stalk you, but I just really REALLY want to
-	see if you would use such a tool. And since you're posting publicly
-	anyway, what do you care if I know you've written some smashing code?
-	(PS: Also, obviously that means you can track what other people are up
-	to and give them some moral support... Cause everyone knows that us
-	programmers need a lot of moral support:
-	<a href="https://twitter.com/search?q=%23githook&src=hash&f=realtime">#githook</a>
-	and note that some of these might not be us.)
+    Also, just a warning, I'm adding a tag #githook so I can track your
+    usage of it. I don't mean to stalk you, but I just really REALLY want to
+    see if you would use such a tool. And since you're posting publicly
+    anyway, what do you care if I know you've written some smashing code?
+    (PS: Also, obviously that means you can track what other people are up
+    to and give them some moral support... Cause everyone knows that us
+    programmers need a lot of moral support:
+    <a href="https://twitter.com/search?q=%23githook&src=hash&f=realtime">#githook</a>
+    and note that some of these might not be us.)
       </p>
       <p>
-	Anyway, just want to say, <b>thanks for all the help!</b>
+    Anyway, just want to say, <b>thanks for all the help!</b>
       </p>
       <a href="<?php echo $redirect_url ?>" style="border: none;">
-	<img src='images/twitter.png' />
+    <img src='images/twitter.png' />
       </a>
       <p>
-	Has only been tested on <span style='color:red;'>Linux</span> and the
-	hook I provide requires <span style='color:red;'>Ruby</span> :)
+    Has only been tested on <span style='color:red;'>Linux</span> and the
+    hook I provide requires <span style='color:red;'>Ruby</span> :)
       </p>
     </div>
   </body>
