6be2b02fd85b39518ea619f4692a66d0e9fefd02
Mr. Snowf1ake <mistahsnowflake@gmail.com>
2013-12-31 12:37:14 -0500

    Replace content for getauthorization.php
    The content and styling for getauthorization.php is replaced for
    consistency with the rest of the site. This includes the inclusion
    of Google web fonts into getauthorization.php, in accordance to the
    rest of the site.
    Consider a template header/footer to be included in the future to be
    more in line with OOP concepts and increase maintainability.

--- src/getauthorization.php
+++ src/getauthorization.php
@@ -21,6 +21,7 @@ if (isset($_REQUEST['oauth_token'])) {
   <head>
     <title>Twitter Git Hook Experiment</title>
     <link rel='stylesheet' type='text/css' href='style/main.css'>
+    <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
     <?php
        if ($redir) {
        echo '<meta http-equiv="refresh" content="5; URL=http://git-hook.mrsnowf1ake.com/generatezip.php" />';
@@ -29,24 +30,25 @@ if (isset($_REQUEST['oauth_token'])) {
   </head>
   <body>
     <div class='container'>
-      <h1>Twitter Git Hook Experiment</h1>
-      <h2>Thanks for being a part of this!</h2>
+      <img src="images/logo.png" class="center">
+      <h1>Twitter Git Hook</h1>
       <?php
      if ($redir) {
      echo '<p><b>Your generated credentials will download in 5 seconds</b></p>';
      }
       ?>
       <p>
-    So, you've gone ahead and downloaded the zip file? Extract the contents
-    into your project's `.git/hooks` directory. Make sure that post-commit is
-    executable (and if it is not, run a <span style='color:blue'>chmod +x post-commit</span>).
+    To install, extract the contents of the zip file into your project's
+    `.git/hooks` directory. Make sure that post-commit is executable (and if it
+    is not, run a <span style='color:blue'>chmod +x post-commit</span>).
     <span style='color:red'> If you already have a post-commit hook, please
-    rename this and call it from your main post-commit.</span>
+    rename the one from Twitter Git Hook and call it from your main
+    post-commit.</span>
       </p>
       <p>
-    Let's reiterate how to use this script... In your git commit message,
-    surround text you want to send to Twitter with `_*` and `*_` like this:
-    <pre>Initialize project _*Working on an awesome new project!*_</pre>
+    To use the script, surround text you wish to send to Twitter with `_*` and
+    `*_` like this:
+    <pre>Initialize project _*Working on a cool new project!*_</pre>
     If you have multiple blocks like that, it will send off multiple posts
     to Twitter. If you make the line too long, the script will split it up
     into multiple posts before sending it off to Twitter. Give it a shot!
